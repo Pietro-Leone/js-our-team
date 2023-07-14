d573a08fabaa6cd6ce9c8571d53bab40118ee239
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-14 15:16:53 +0200

    inizio esercizio

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const teamMember = [
+    {
+        firstName: "Wayne" ,
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Founder & CEO",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    }
+]
+
+console.log(teamMember);
